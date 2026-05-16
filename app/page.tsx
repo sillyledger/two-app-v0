055c1ba8ec64d84fd2e04552606dcfedd03c2d28
sillyledger <tenkaro@icloud.com>
2026-05-16 13:45:09 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
-
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import useSWR from 'swr'
 import { Sidebar } from '@/components/sidebar'
