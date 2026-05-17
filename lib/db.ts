21ddbcc42d172d1a36681f6ea8a62fed118c2590
sillyledger <tenkaro@icloud.com>
2026-05-17 17:53:16 +0800

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,11 +2,12 @@ import { neon } from '@neondatabase/serverless'
 
 export const sql = neon(process.env.DATABASE_URL!)
 
-export interface Note {
+export interface Doc {
   id: number
   title: string
   content: string | null
   color: string
+  type: string
   is_starred: boolean
   created_at: string
   updated_at: string
