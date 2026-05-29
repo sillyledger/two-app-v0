7855af1a47f506bb936a30ccf6e237c1f640676d
sillyledger <tenkaro@icloud.com>
2026-05-30 07:47:25 +0800

    Update liveblocks.ts

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@liveblocks/client"
 import { createRoomContext } from "@liveblocks/react"
 
-export const client = createClient({
+const client = createClient({
   authEndpoint: async (room) => {
     const response = await fetch("/api/liveblocks-auth", {
       method: "POST",
