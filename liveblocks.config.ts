7c12f327205dd459fd0ccd3ba15628b7d32bef8a
sillyledger <tenkaro@icloud.com>
2026-06-07 10:18:31 +0800

    Update liveblocks.config.ts

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@liveblocks/client"
 import { createRoomContext } from "@liveblocks/react"
+import { LiveObject } from "@liveblocks/client"
 
 const client = createClient({
   authEndpoint: "/api/liveblocks-auth",
@@ -10,11 +11,16 @@ type Presence = {
   color: string
 }
 
-type Storage = {}
+type Storage = {
+  content: LiveObject<{ html: string }>
+}
 
 export const {
   RoomProvider,
   useOthers,
   useSelf,
   useRoom,
+  useStorage,
+  useMutation,
+  useIsStorageLoading,
 } = createRoomContext<Presence, Storage>(client)
