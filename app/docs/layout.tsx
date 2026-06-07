859e94a6f1e5d504b22ff2c8a33e5e3201b3f255
sillyledger <tenkaro@icloud.com>
2026-06-07 19:39:52 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -38,8 +38,8 @@ export default function DocsLayout({ children }: { children: React.ReactNode }) 
         onToggle={() => setCollapsed(v => !v)}
       />
       <div
-        className="flex flex-col min-w-0 transition-all duration-300 ease-in-out"
-        style={{ marginLeft: sidebarWidth, ['--sidebar-width' as string]: sidebarWidth, width: `calc(100% - ${sidebarWidth})` }}
+        className="flex-1 flex flex-col min-w-0 transition-all duration-300 ease-in-out"
+        style={{ marginLeft: sidebarWidth, ['--sidebar-width' as string]: sidebarWidth }}
       >
         <TabBar />
         {children}
