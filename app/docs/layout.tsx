ce6c27747275b1600c1f0f762f77896701114d87
sillyledger <tenkaro@icloud.com>
2026-06-07 12:34:36 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -39,9 +39,9 @@ export default function DocsLayout({ children }: { children: React.ReactNode }) 
       />
       <div
         className="flex-1 flex flex-col min-w-0 transition-all duration-300 ease-in-out"
-        style={{ marginLeft: sidebarWidth }}
+        style={{ marginLeft: sidebarWidth, ['--sidebar-width' as string]: sidebarWidth }}
       >
-        <TabBar sidebarWidth={sidebarWidth} />
+        <TabBar />
         {children}
       </div>
     </div>
